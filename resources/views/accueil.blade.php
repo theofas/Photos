6772e75b1ee0f072bec82aa8b7837a348ab8490a
caron <lucas_caron@ens.univ-artois.fr>
2024-11-21 12:31:12 +0100

    style page accueil.blade.php

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -2,15 +2,32 @@
 <link href="{{  asset('/css/style.css')  }}" rel="stylesheet" />
 
 
+<nav>
+    <a href="{{route('accueil')}}">Accueil</a>
+    <a href="{{route('albums')}}">Album</a>
+    <input type="text" name="recherche">
+    <a href="">Login</a>
+</nav>
 
 
-
-<h1>Voici la page d'accueil </h1>
-
-<a href="{{route("albums")}}">album</a>
+<h1>Mon Album photo VACANCES</h1>
+<h3>" Collecter facilement et rapidement vos plus beaux souvenirs de vacances chez nous, et transformez-les en moments inoubliables à partager</h3>
 
 
-<a href="{{route("tags", 'vacances')}}">vacances</a>
-<a href="{{route("tags", 'neige')}}">neiges</a>
-<a href="{{route("tags", 'mer')}}">mer</a>
-<a href="{{route("tags", 'repas')}}">repas</a>
+<div class="cards">
+    <div class="card1">
+        <a href="{{route('tags', 'vacances')}}">vacances</a>
+    </div>
+
+    <div class="card2">
+        <a href="{{route('tags', 'neige')}}">neiges</a>
+    </div>
+
+    <div class="card3">
+        <a href="{{route('tags', 'mer')}}">mer</a>
+    </div>
+
+    <div class="card4">
+        <a href="{{route('tags', 'repas')}}">repas</a>
+    </div>
+</div>
